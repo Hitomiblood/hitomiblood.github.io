2e5353921ac6be46cda32e218886aa1dc9d2bcb0
Miguel Santiago Gómez Suárez <Hitomiblood@gmail.com>
2025-06-08 16:46:51 -0500

    brak test

--- test/app.test.js
+++ test/app.test.js
@@ -5,6 +5,6 @@ describe('GET /', () => {
   it('should return HTML', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toBe(200);
-    expect(res.text).toContain('Miguel Gómez Page');
+    expect(res.text).toContain('Miguel Gómez Page 2');
   });
 });
